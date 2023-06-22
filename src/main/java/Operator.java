b10e83c96a7eaec905345fee9f4bfa71e99e47fa
Diana Yavorska <yavorska.diana91@gmail.com>
2023-06-22 15:25:13 +0300

    first commit

---
+++ src/main/java/Operator.java
@@ -0,0 +1,14 @@
+public class Operator
+
+{ public static void main(String[] args) {
+
+    int globalVal = 234;
+    int globalVal1 = 150;
+
+    int systemVal = 56;
+    int systemVal1 = 98;
+
+
+}
+}
+
